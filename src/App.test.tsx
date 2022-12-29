2896001bcfbceefc4c5e9dee7ee050ce4c059d64
Jakub Sieradzki <j.sieradzki45@gmail.com>
2022-12-29 17:17:50 +0100

    file structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './view/App.view';
 
 test('renders learn react link', () => {
   render(<App />);
